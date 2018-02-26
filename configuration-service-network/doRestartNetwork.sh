58635a736a0639ad86979c185cbd38a5fa6d00a3
Massimiliano Alberti <MassiAlberti>
2018-02-26 14:53:18 +0100

    BugFix: New file doRestartNetwork.sh

---
+++ configuration-service-network/doRestartNetwork.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "WARNING!!! ALL Fabric Data will be destroyed"
+docker volume rm $(docker volume ls)
+docker rmi $(docker images | grep "smartfactory")
+rm -rf ~/data/* && ./byfn.sh -m up -c ledgerchannel -s couchdb
